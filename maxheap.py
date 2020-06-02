68a2d179aeada4258bb7ad20a82a85e590724ef6
mightnent <mightnent@gmail.com>
2020-06-02 22:02:06 +0800

    fixed code

--- maxheap.py
+++ maxheap.py
@@ -19,48 +19,60 @@ class MaxHeap():
                         break
     
     def remove(self):
-        biggest = self.heap[1]
-        if len(self.heap) > 2:
-            self.heap[1] = self.heap.pop()
-            if len(self.heap) ==3:
-                if self.heap[1] > self.heap[2]:
-                    tmp = self.heap[1]
-                    self.heap[1] = self.heap[2]
-                    self.heap[2] = tmp
-                return biggest
-            i = 1
-            left = 2 * i
-            right = 2 * i + 1
-            while(self.heap[i] <= self.heap[left] or self.heap[i] <= self.heap[right]):
-                if self.heap[left] > self.heap[right]:
-                    tmp = self.heap[i]
-                    self.heap[i] = self.heap[left]
-                    self.heap[left] = tmp
-                    i = 2 * i
-                else:
-                    tmp = self.heap[i]
-                    self.heap[i] = self.heap[right]
-                    self.heap[right] = tmp
-                    i = i *2 +1
-
-                left = 2 * i
-                right = 2 * i +1
-
-                '''
-                We can do perform the following check because the last element we pop and 
-                assign to first index is not the smallest
-                '''
-                try:
-                    self.heap[left] or self.heap[right]
-                except:
-                    break
+        try:
+            biggest = self.heap[1]
+            if len(self.heap) > 2:
+                self.heap[1] = self.heap.pop()
+                if len(self.heap) == 2:
+                    return biggest
+                if len(self.heap) == 3:
+                    if self.heap[1] < self.heap[2]:
+                        tmp = self.heap[1]
+                        self.heap[1] = self.heap[2]
+                        self.heap[2] = tmp
                 
-        elif len(self.heap) == 2:
-            self.heap.pop()
-        else:
-            return None
-        return biggest
+                    return biggest
+                i = 1
+                left = 2 * i
+                right = 2 * i + 1
+                while(self.heap[i] <= self.heap[left] or self.heap[i] <= self.heap[right]):
+                
+                    if self.heap[left] > self.heap[right]:
+                        tmp = self.heap[i]
+                        self.heap[i] = self.heap[left]
+                        self.heap[left] = tmp
+                        i = 2 * i
+                    else:
+                        tmp = self.heap[i]
+                        self.heap[i] = self.heap[right]
+                        self.heap[right] = tmp
+                        i = i *2 +1
 
+                    left = 2 * i
+                    right = 2 * i +1
+                
+
+                    '''
+                    We can do perform the following check because the last element we pop and 
+                    assign to first index is not the smallest
+                    '''
+                    
+                    try:
+                        self.heap[left] or self.heap[right]
+                    except IndexError:
+                    
+                        break
+
+            elif len(self.heap) == 2:
+                self.heap.pop()
+            else:
+    
+                return None
+        
+            return biggest
+        except:
+            return "no more"
+            
     def sort(self):
         result = []
         while(len(self.heap)>1):
